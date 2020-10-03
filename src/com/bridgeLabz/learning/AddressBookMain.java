30c187ddd95b2fd11c26f018ce84389a21644857
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-03 17:22:08 +0530

    [Umesh]Added: adding new contact to addressbook

--- src/com/bridgeLabz/learning/AddressBookMain.java
+++ src/com/bridgeLabz/learning/AddressBookMain.java
@@ -4,26 +4,42 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to addressBook problem");
 		Scanner sc= new Scanner(System.in);
+		System.out.println("Welcome to addressBook problem");
 		Persons addressBook = new Persons();
-		System.out.println("enter first name");
-		String firstName =sc.next();
-		System.out.println("enter last name");
-		String lastName =sc.next();
-		System.out.println("enter address");
-		String address =sc.next();
-		System.out.println("enter city");
-		String city =sc.next();
-		System.out.println("enter state");
-		String state =sc.next();
-		System.out.println("enter zip");
-		int zip =sc.nextInt();
-		System.out.println("enter mobile number");
-		Long mobNum =sc.nextLong();
-		System.out.println("enter email");
-		String email =sc.next();
-		Details p = new Details(firstName, lastName, address, city, state, zip, mobNum, email);
+		while(true){
+			System.out.println("1. add contacts\n 2.exit");
+			System.out.println("Enter your choice");
+			int n=sc.nextInt();
+			switch(n){
+			case 1:
+				System.out.println("enter first name");
+				String firstName =sc.next();
+				System.out.println("enter last name");
+				String lastName =sc.next();
+				System.out.println("enter address");
+				String address =sc.next();
+				System.out.println("enter city");
+				String city =sc.next();
+				System.out.println("enter state");
+				String state =sc.next();
+				System.out.println("enter zip");
+				int zip =sc.nextInt();
+				System.out.println("enter mobile number");
+				Long mobNum =sc.nextLong();
+				System.out.println("enter email");
+				String email =sc.next();
+				addressBook.addContacts(firstName, lastName, address, city, state, zip, mobNum, email);
+				break;
+			case 2:
+				System.exit(0);
+			}
+		}
+		
+		
+		
+		
+
 
 	}
 }
--- src/com/bridgeLabz/learning/Persons.java
+++ src/com/bridgeLabz/learning/Persons.java
@@ -11,5 +11,10 @@ public class Persons {
 			// TODO Auto-generated constructor stub
 		}
 		
-		
+		public void addContacts(String firstName, String lastName, String address, String city, String state, int zip, Long mobNum,
+				String email){
+			Details c = new Details(firstName,lastName, address, city, state, zip,mobNum,email);
+			contacts.add(c);
+		}
+
 }
