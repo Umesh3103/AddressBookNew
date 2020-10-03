4b9bac4e26ba82f26e1b405a69e9abdf921acfb0
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-03 17:54:27 +0530

    [Umesh]Added: creating multiple addressbook

--- src/com/bridgeLabz/learning/AddressBookMain.java
+++ src/com/bridgeLabz/learning/AddressBookMain.java
@@ -2,55 +2,96 @@ package com.bridgeLabz.learning;
 
 import java.util.Scanner;
 
-public class AddressBookMain {
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Welcome to addressBook problem");
-		Persons addressBook = new Persons();
-		while(true){
-			System.out.println("1. add contacts\n 2.edit contacts\n 3.view contacts\n 4.delete contacts\n5.exit");
-			System.out.println("Enter your choice");
-			int n=sc.nextInt();
-			switch(n){
-			case 1:
-				System.out.println("enter first name");
-				String firstName =sc.next();
-				System.out.println("enter last name");
-				String lastName =sc.next();
-				System.out.println("enter address");
-				String address =sc.next();
-				System.out.println("enter city");
-				String city =sc.next();
-				System.out.println("enter state");
-				String state =sc.next();
-				System.out.println("enter zip");
-				int zip =sc.nextInt();
-				System.out.println("enter mobile number");
-				Long mobNum =sc.nextLong();
-				System.out.println("enter email");
-				String email =sc.next();
-				addressBook.addContacts(firstName, lastName, address, city, state, zip, mobNum, email);
-				break;
-			case 2:
-				System.out.println("Enter the name for which you wants to edit contacts");
-				String name = sc.next();
-				addressBook.editContacts(name);
-				break;
-			case 3:
-				addressBook.viewContacts();
-				break;
-			case 4:
-				String s = sc.next();
-				int i=addressBook.deleteContacts(s);
-				if(i==0){
-					System.out.println("Given contact does not exist");
-				}
-				break;
-				
-			case 5:
-				System.exit(0);
-			}
-		}
 
+import java.util.*;
+
+public class AddressBookMain {
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Persons addressBook = new Persons();
+		Persons addressBook2 = new Persons();
+		int flag;
+		
+		while(true){
+			System.out.println("In which addressBook you wants to add details. Press 0 for addressBook1 and 1 for addressBook2");
+			flag= sc.nextInt();
+			System.out.println("1. add contacts\n 2. edit contacts\n 3. delete contacts\n 4. viewContacts\n 5. exit.");
+			int n;
+			System.out.println("Enter your choice");
+			n=sc.nextInt();
+			System.out.println("");
+		switch(n){
+		case 1:
+			System.out.println("Create new contacts");
+			System.out.println("enter first name");
+			String firstName =sc.next();
+			System.out.println("enter last name");
+			String lastName =sc.next();
+			System.out.println("enter address");
+			String address =sc.next();
+			System.out.println("enter city");
+			String city =sc.next();
+			System.out.println("enter state");
+			String state =sc.next();
+			System.out.println("enter zip");
+			int zip =sc.nextInt();
+			System.out.println("enter mobile number");
+			Long mobNum =sc.nextLong();
+			System.out.println("enter email");
+			String email =sc.next();
+			if(flag==0){
+			addressBook.addContacts(firstName, lastName, address, city, state, zip, mobNum, email);
+			}
+			else{
+				addressBook2.addContacts(firstName, lastName, address, city, state, zip, mobNum, email);
+			}
+			break;
+		
+		case 2:
+			System.out.println("Enter the name for which you wants to edit contacts");
+			String name = sc.next();
+				if(flag==0){
+				addressBook.editContacts(name);
+				}
+				else{
+					addressBook2.editContacts(name);
+				}
+				System.out.println("Edited contacts");
+				addressBook.viewContacts();
+			
+			break;
+			
+		case 3:
+			String s = sc.next();
+			int i;
+			if(flag==0){
+			i = addressBook.deleteContacts(s);
+			}
+			else{
+				i=addressBook2.deleteContacts(s);
+			}
+			if(i==0){
+				System.out.println("Given contact does not exist");
+			}
+			break;
+		case 4:
+			if(flag==0){
+				addressBook.viewContacts();
+			}
+			else{
+				addressBook2.viewContacts();
+			}
+			break;
+			
+		case 5:
+			System.exit(0);
+			break;
+		}
+		
+		}	
 	}
+
 }
+
